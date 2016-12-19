8ca1dc88892b445abbc3fc2682db5f6787de35c6
Piotr Gąsiorowski <gasiorowski.piotr@windowslive.com>
2016-12-19 22:33:49 +0100

    Port setup

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import * as http from 'http';
 
+const port = process.env.port || 80;
+
 const server = http.createServer((request: http.IncomingMessage, response: http.ServerResponse) => {
 
     response.write("Hello world!");
     response.end();
 
-}).listen(process.env.port);
+}).listen(port);
